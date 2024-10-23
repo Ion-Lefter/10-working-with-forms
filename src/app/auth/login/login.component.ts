c6fe50f622acb5c7d22ecf0788e8955d9ac5612b
Ion Lefter <ionlefter1997@gmail.com>
2024-10-23 15:54:10 +0300

    247. Reactive Forms: Getting Started

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,7 +1,6 @@
-import { NgFor } from '@angular/common';
-import { afterNextRender, Component, DestroyRef, inject, viewChild } from '@angular/core';
-import { FormsModule, NgForm, NgModel } from '@angular/forms';
-import { debounceTime } from 'rxjs';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
@@ -11,44 +10,10 @@ import { debounceTime } from 'rxjs';
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  myForm = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl('')
+  });
 
-  private form = viewChild.required<NgForm>('form');
-  private destroyRef = inject(DestroyRef);
-
-  constructor(){
-    afterNextRender(() => {
-      const savedForm = window.localStorage.getItem('saved-login-form');
-      if(savedForm){
-        const loadedFormData = JSON.parse(savedForm);
-        const savedEmail = loadedFormData.email;
-        setTimeout( () => {
-          this.form().controls['email'].setValue({
-            email: savedEmail,
-            password: ''
-          });
-        }, 1);
-      }
-
-      const subscription = this.form().valueChanges?.pipe(debounceTime(500)).subscribe({
-        next: (value) => window.localStorage.setItem('saved-login-form', 
-          JSON.stringify({email: value.email})
-      ),
-     });
-      this.destroyRef.onDestroy(() => subscription?.unsubscribe());
-    });
-  }
-
-  onSubmit(formData: NgForm){
-    if(formData.form.invalid){
-      return;
-    }
-    const enteredEmail = formData.form.value.email;
-    const enteredPassword = formData.form.value.password;
-    console.log(formData);
-    console.log(enteredEmail);
-    console.log(enteredPassword);
-
-    formData.form.reset();
-
-  }
+  onsubmit(){}
 }
